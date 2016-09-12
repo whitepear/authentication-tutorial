9b3f760d2a562ee6e5cfc047409f681403eb3b6d
John Doe <webdevnorth@gmail.com>
2016-09-12 16:43:30 +0100

    implemented password hashing via bcryptjs

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 var UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
@@ -19,6 +20,28 @@ var UserSchema = new mongoose.Schema({
 		type: String,
 		required: true
 	}
+});
+
+// Mongoose offers a pre-save hook: essentially, a function
+// that runs before saving a record to Mongo
+// hash password before saving to database.
+// call pre method on our schema, 2 params: hook name, function 
+// In this case hook name is save, which is a special Mongoose keyword
+// This is run before saving the record
+// next represents the next function, which in this case is saving the record to mongo
+// 'this' is assigned the database object Mongoose is about to insert into Mongo
+UserSchema.pre('save', function (next) {
+	var user = this;
+	// hash takes 3 args: plaintext pass, num, callback (called after hash gen)
+	// num tells bcrypt how many times to apply the encryption algorithm
+	// Bigger numbers, more security, slower to gen
+	bcrypt.hash(user.password, 10, function(err, hash) {
+		if (err) {
+			return next(err);
+		}
+		user.password = hash;
+		next();	// calling next calls the next function in the middleware stack - in this case, mongoose will save the data to mongo
+	});
 });
 
 var User = mongoose.model('User', UserSchema);
