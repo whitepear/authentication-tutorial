fb3dd3537e712c470c781e5627c9023a50aa8d2d
John Doe <webdevnorth@gmail.com>
2016-09-13 01:40:39 +0100

    created requiresLogin middleware

--- middleware/index.js
+++ middleware/index.js
@@ -5,4 +5,14 @@ function loggedOut(req, res, next) {
 	return next();
 }
 
+function requiresLogin(req, res, next) {
+	if (req.session && req.session.userId) {
+		return next();
+	}
+	var err = new Error('You are not authorized to view this page.');
+	err.status = 403; // 'forbidden'
+	return next(err);
+}
+
+module.exports.requiresLogin = requiresLogin;
 module.exports.loggedOut = loggedOut;
